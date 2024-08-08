d88dd2e7cf483037388450c871483bcbd0af483d
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-08-08 16:33:10 +0530

    user and video model with JWT

---
+++ src/models/user.models.js
@@ -0,0 +1,97 @@
+import mongoose , {Schema} from "mongoose";
+import jwt from "jsonwebtoken" //JWT
+import bcrypt from "bcrypt" //bcrypt the file
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true // if u want to make a field to be more searchable use index
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String,// cloudnary URL
+        required:true,
+    },
+    coverImage:{
+        type:String, // cloudnary URL
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,"password is Required"] // custome message
+    },
+    refreshToken:{
+        type:String
+    }
+},
+{
+    timestamps:true
+})
+
+/* here we have pre, which is mongooose hook where we can make execution of certain action before certain event
+like here we have "save" event, now we can make some action on the schema before saving like
+bcrypting the password */
+
+//here we should use function rather then ()=>{} because anonymous func dont get reference of this
+//
+userSchema.pre("save",async function(next){
+    // using isModified because the lower code will run again and again whenver user will make some changes in its model
+    // so we should check if the password is modified or not 
+    if(!this.isModified("password")) return next(); 
+    this.password = bcrypt.hash(this.password,10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User = mongoose.model("User",userSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose , {Schema} from "mongoose";
+import mongooseAggregatePeginate  from 'mongoose-aggregate-paginate-v2';
+
+const videoSchema  = new Schema(
+    {
+        videosFile:{
+            type:String, //cloudinary url
+            required:true 
+        },
+        thumbnail:{
+            type:String, //cloudinary url
+            required:true 
+        },
+        title:{
+            type:String, 
+            required:true 
+        },
+        description:{
+            type:String, 
+            required:true 
+        },
+        duration:{
+            type:Number, // Cloudniary, we get the duation of videos from cloud 
+            required:true 
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        published:{
+            type:Boolean,
+            default:true
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+
+    }
+    
+,{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatePeginate)
+
+export const Video = mongoose.model("Video",videoSchema) 
